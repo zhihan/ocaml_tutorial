4856ff56ceb94c3531fa3bb3c6e0c8ad2992c998
Zhi Han <zhi.han@gmail.com>
2013-02-28 22:08:15 -0500

    Added trim string from ocaml 4

--- tools/readfile.ml
+++ tools/readfile.ml
@@ -1,19 +1,38 @@
+(* Trim leading and trailing spaces *)
+let str_trim s =
+  let is_space = function
+    | ' ' | '\012' | '\n' | '\r' | '\t' -> true
+    | _ -> false in
+  let len = String.length s in
+  let i = ref 0 in
+  while !i < len && is_space (String.get s !i) do
+    incr i
+  done;
+  let j = ref (len - 1) in
+  while !j >= !i && is_space (String.get s !j) do
+    decr j
+  done;
+  if !i = 0 && !j = len - 1 then
+    s
+  else if !j >= !i then
+    String.sub s !i (!j - !i + 1)
+  else
+    ""
 
+(* Process a line and separate the two sides of '=' *)
 let process_line (line:string) = 
   if String.contains line '=' then
     let i = String.index line '=' in
-    let lhs = String.sub line 1 (i-1) in
+    let lhs = String.sub line 0 i in
     let rhslen  = (String.length line) - i - 1 in
     let rhs = String.sub line (i+1) rhslen in
-    (lhs, rhs)
+   (str_trim lhs, str_trim rhs)
   else
     ("", "")
 
-  
+(* Read input and divide the lines separated by '=' *)  
 let read_inputs filename = 
   let ic = open_in filename  in
-  (* let ocamlroot = ref "" in
-  let proverlib = ref "" in *)
   try
     while true  do
       let line = input_line ic in
